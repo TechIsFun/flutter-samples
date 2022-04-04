3a28a42ffc35ae8bde9fc40dfefcaca77d85c249
Andrea Maglie <andrea.maglie@gmail.com>
2022-04-04 20:52:26 +0200

    chore: fix formatting issue

--- dynamicinitialroute/lib/main.dart
+++ dynamicinitialroute/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-      /*
+    /*
       return MaterialApp(
           title: 'Dynamic initial route Demo',
           theme: ThemeData(
