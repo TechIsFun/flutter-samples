a497abb3a16761d531007b2ce1a7b385669dda5a
Andrea Maglie <andrea.maglie@gmail.com>
2022-04-04 15:08:33 +0200

    feat: add 'connectionrequired' sample project

---
+++ connectionrequired/test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:connectionrequired/main.dart';
+
+void main() {
+  testWidgets('Simple widget test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+  });
+}
